fc47a496a5da8827a4bf97df871b6f8294fe2b0b
ElenaZaharia30 <118385360+ElenaZaharia30@users.noreply.github.com>
2025-06-26 17:08:48 +0300

    update dependancies

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,4 +4,14 @@
     <header>
         <p>This is a simple starter kit for Laravel 11, Inertia JS, and Vue JS, without authentication. Tailwind and ZiggyVue are installed.</p>
     </header>
+    <!-- example.blade.php -->
+    <div class="p-6">
+        <label class="block text-sm font-medium text-gray-700">Email</label>
+        <input
+            type="email"
+            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+            placeholder="you@example.com"
+        />
+    </div>
+
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                montserrat: ['Montserrat', 'ui-sans-serif', 'system-ui']
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
 }
-
